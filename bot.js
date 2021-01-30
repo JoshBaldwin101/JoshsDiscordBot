53f449153f2cbeb658d4e22178027c5fe081d967
Josh Baldwin <jmbaldwin@sdsu.edu>
2021-01-30 03:56:45 -0800

    Changed command symbol to '+' avoid conflicts with other bots. Changed parameters for almost all methods. Command recognition is working now. Bot automatically connects to voice chat upon any command.

--- bot.js
+++ bot.js
@@ -34,7 +34,7 @@ client.on('ready', () => {
 client.login(process.env.DISCORD_TOKEN);
 
 // All commands must begin with this symbol:
-var commandSymbol = '!'; // Example: "!help", "!echo Hello World!"
+var commandSymbol = '+'; // Example: "!help", "!echo Hello World!"
 
 // List of commands -- keep these in lowercase!
 var helpCommandStr = "help";
@@ -44,12 +44,29 @@ var connectCommandStr = "connect";
 var disconnectCommandStr = "disconnect";
 var statusCommandStr = "status";
 
-client.on('message', msg => {	// This code block runs when a user sends a Discord message anywhere that the bot can read
-	// Do stuff when a message is sent
+// Other variables
+var attemptedCommand; // String for the attempted command
+
+client.on('message', async msg => {	// This code block runs when a user sends a Discord message anywhere that the bot can read
+
+	if(msg.content.substring(0,1) != commandSymbol) // Check if msg was even a command - REMEMBER: this code runs EVERY time a message is sent in the server!
+	{
+		return; // No command detected, break ASAP
+	}
+	else
+	{
+		attemptedCommand = msg.content.split(" ")[0].substring(commandSymbol.length, msg.content.split(" ")[0].length) // attemptedCommand = the command minus the ! symbol
+
+		if (msg.member.voice.channel) {		// TODO: Delete this code when you figure out how to relocate to an outside function
+			const connection = await msg.member.voice.channel.join();
+		}
+	}
 
 	// Switch statement to figure out which command they used and to call the appropriate method
 	switch(attemptedCommand)
 	{
+		case null:
+			break;
 		case helpCommandStr:
 			help(msg);
 			break;
@@ -69,35 +86,19 @@ client.on('message', msg => {	// This code block runs when a user sends a Discor
 			status(msg);
 			break;
 		default:
-			unrecognizedCommand(msg);
+			unrecognizedCommand(msg.content);
 			break;
 	}
 })
 
 /**
- * This function takes in the entire message sent by a user then returns the command invocation (Example: User says "!echo Hello World" this func returns "!echo")
- * @param	{String}	msg		The message sent by a user
- * @return	{String}			Returns only the command invocation
- */
-function isolateCommandInvocation(msg)
-{
-	// If there aren't any spaces that means no arguments were passed, therefore, no point in splitting
-	if (msg.indexOf(' ')) // Check for spaces
-	{
-		return msg.toLowerCase();
-	}
-
-	return msg.split(" ")[0].toLowerCase(); // Returns the first element in the array which would be the command invocation
-}
-
-/**
  * This function takes in the entire message sent by a user then returns the content in a String minus the command invocations (Example: User says "!echo Hello World" this func returns "Hello World")
- * @param	{String}	msg		The message sent by a user
+ * @param	{Object}	msg		The message sent by a user
  * @return	{String}			Returns the message sent by a user, but without the command invocation
  */
 function cutOutArguments(msg)
 {
-	return content;
+	return null;
 }
 
 
@@ -116,36 +117,37 @@ function alarm()
 
 /**
  * This function takes in the entire message sent by a user then returns the content in a String minus the command invocations (Example: User says "!echo Hello World" this func returns "Hello World")
- * @param	{String}	msg		The message sent by a user
+ * @param	{Object}	msg 		The actual message object sent by a user
  */
-function echo(msg)
+function echo(content, msg)
 {
-	msg = cutOutArguments(msg);
-	msg.channel.send(msg);
+	content = cutOutArguments(content);
+	msg.channel.send(content);
 }
 
 /**
  * This function sends a message informing users of what commands they can use and how to use them + includes an MOTD too
+ * @param	{Object}	msg			The actual message object sent by a user
  */
-function help()
+function help(msg)
 {
 	// Print the help list
-	msg.channel.send('Hello! This bot is a work in progress. Contact my creator, X3liteNinjaX#6891, for more info!');
+	msg.channel.send('Hello! This bot is a work in progress. Contact my creator, @X3liteNinjaX#6891, for more info!');
 }
 
 /**
  * This function updates the Discord bot's status with a given argument
- * @param	{String}	msg		The message sent by a user
+ * @param	{Object}	msg		The message content sent by a user
  */
 function status(msg)
 {
 	// Sets the bot's Discord status
-	client.user.setActivity("Testing this code"); 
+	client.user.setActivity("something"); 
 }
 
 /**
  * This function informs the user that their attempted command was not recognized
- * @param	{String}	msg		The message sent by a user
+ * @param	{Object}	msg		The message sent by a user
  */
 function unrecognizedCommand(msg)
 {
@@ -154,16 +156,32 @@ function unrecognizedCommand(msg)
 
 /**
  * This function connects the bot to the same voice channel as the user invoking this command
- * @param	{String}	msg		The message sent by a user
+ * @param	{Object}	msg		The message sent by a user
  */
 function voiceConnect(msg)
 {
+	if (msg.member.voice.channel) {
+		const connection = msg.member.voice.channel.join();
+	}
+
+
+	/*
 	// Connect the bot to a voice channel
+	client.channels.cache.get("ChannelID")
+  	if (!channel) return console.error("The channel does not exist!");
+  	channel.join().then(connection => {
+    // Yay, it worked!
+    	console.log("Successfully connected.");
+  	}).catch(e => {
+    // Oh no, it errored! Let's log it to console :)
+    console.error(e);
+	});
+	*/
 }
 
 /**
  * This function disconnects the bot from the voice channel
- * @param	{String}	msg		The message sent by a user
+ * @param	{Object}	msg		The message sent by a user
  */
 function voiceDisconnect(msg)
 {
